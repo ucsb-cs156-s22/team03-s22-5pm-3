5a9899c5c8a55655255e14d865a2d7ebe70022a7
Samuel Z Guo <samuelzguo@Eureka.local>
2022-05-18 01:08:23 -0700

    Sam - fixed component and fixture

---
+++ frontend/src/main/components/Recommendations/RecommendationsTable.js
@@ -0,0 +1,80 @@
+import OurTable, { ButtonColumn} from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import {  onDeleteSuccess } from "main/utils/RecommendationsUtils"
+// import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/recommendations",
+        method: "DELETE",
+        params: {
+            code: cell.row.values.code
+        }
+    }
+}
+
+export default function RecommendationsTable({ recommendations, currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/recommendations/all"]
+    );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'Requester Email',
+            accessor: 'requesterEmail', 
+        },
+        {
+            Header: 'Professor Email',
+            accessor: 'professorEmail',
+        },
+        {
+            Header: 'Explanation',
+            id: 'explanation',
+        },
+        {
+            Header: 'Date Requested',
+            id: 'dateRequested',
+        },
+        {
+            Header: 'Date Needed',
+            id: 'dateNeeded',
+        },
+        {
+            Header: 'Done',
+            accessor: 'done',
+            accessor: (row, _rowIndex) => String(row.hasDiningCam)
+        }
+    ];
+
+    const testid = "RecommendationsTable";
+
+    const columnsIfAdmin = [
+        ...columns,
+        // ButtonColumn("Edit", "primary", editCallback, testid),
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={recommendations}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
