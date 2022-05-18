051b36eab1f8791546c11f168015c06e136a0b72
Samuel Z Guo <samuelzguo@169-231-95-191.wireless.ucsb.edu>
2022-05-17 20:29:21 -0700

    Sam - add recommendations placeholder index page

---
+++ frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function RecommendationsIndexPage() {
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <h1>Recommendations</h1>
+                <p>
+                    Recommendations placeholder page
+                </p>
+            </div>
+        </BasicLayout>
+    )
+}
