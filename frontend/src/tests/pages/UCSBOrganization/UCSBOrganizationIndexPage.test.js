8dd3c43c82d3d513b4eb5c1d7e0ae7ba80e250b3
jwangathan <jonathan_wang@umail.ucsb.edu>
2022-05-18 23:34:52 -0700

    jw - fixed unused vars

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationIndexPage.test.js
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationIndexPage.test.js
@@ -8,7 +8,7 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import mockConsole from "jest-mock-console";
+import _mockConsole from "jest-mock-console";
 
 
 const mockToast = jest.fn();
